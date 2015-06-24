66f7c3cae953077c11953e16749162d4b7a2e369
Adolf0 <adolfo.serrano@gmail.com>
2015-06-24 12:15:42 +0200

    “Despliegue_en_Heroku”

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var partials =  require('express-partials');
 var routes = require('./routes/index');
 //aqui
 //var users = require('./routes/users');
@@ -15,7 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
+// uncomment after placing your favicon in /publicpa
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -24,6 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use(partials());
 //aqui
 //app.use('/users', users);
 
